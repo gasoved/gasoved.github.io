5be0a0b12bb1594de60a2b85c3038f4d3d5b586d
mehmetemir <mehmet@emircetinkaya.com>
2014-10-13 02:57:15 +0300

    First public version

---
+++ js/script.js
@@ -0,0 +1,8 @@
+(function() {
+    var po = document.createElement("script");
+    po.type = "text/javascript";
+    po.async = true;
+    po.src = ("https:" == document.location.protocol ? "https" : "http") + "://www.freelancer.com/js/hireme/widget.js";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(po, s);
+})();
