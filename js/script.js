a5e6410ce09b16fbeb174fd6d6f9093a2ec0396a
Fkrmb <gasoved@gmail.com>
2017-01-01 22:39:39 +0300

    Changes

--- js/script.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    var po = document.createElement("script");
-    po.type = "text/javascript";
-    po.async = true;
-    po.src = ("https:" == document.location.protocol ? "https" : "http") + "://www.freelancer.com/js/hireme/widget.js";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(po, s);
-})();
